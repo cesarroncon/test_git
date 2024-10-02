d3ac84331d95da31f8eb6afc3039d12b7f98faeb
cesar roncon <cesarroncon@jmdr.pt>
2024-10-02 17:49:46 +0100

    cool wave

--- teste.php
+++ teste.php
@@ -1,2 +1,32 @@
 <?php 
+$count = 0;
+$str = [];
+while ($count < 10000) {
+    $i = 0;
+    $d = 100;
+
+    while ($i < 100) {
+        $str[$i] = '*';
+        $j = 0;
+        while ($j < $i) {
+            echo $str[$j];
+            $j++;
+        }
+        echo "\n";
+        $i++;
+        usleep(50000);
+    }
+    while ($d > 0) {
+        $str[$d] = '';
+        $k = 0;
+        while ($k < $d) {
+            echo $str[$k];
+            $k++;
+        }
+        echo "\n";
+        $d--;
+        usleep(50000);
+    }
+    $count++;
+}
 echo "Hello World";
